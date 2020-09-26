745d7ccd17ec686776a71a2a04b37bb46985761b
Sarath P S <sarathps93@gmail.com>
2020-09-26 14:37:12 +0530

    fix(*): a small correction in insertion sort

--- sorting/insertionSort/insertionSort.js
+++ sorting/insertionSort/insertionSort.js
@@ -2,7 +2,7 @@ function insertionSort(arr){
 	let currentVal;
     for(let i = 1; i < arr.length; i++){
         currentVal = arr[i];
-        for(let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
             arr[j+1] = arr[j]
         }
         arr[j+1] = currentVal;
